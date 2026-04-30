e5b45327f602999567ada14620e51f96e8fc71e9
Wsylq <sylqw@atomicmail.io>
2026-04-30 17:22:53 +0530

    untracked data json file

--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,7 @@ module.exports = {
   once: true,
   execute(client, bot) {
     console.log(`[Bot] Logged in as ${client.user.tag}`);
-    client.user.setActivity("music 🎵", { type: 2 });
+    client.user.setActivity("Assassin is listening to his Daddy Lossai 🎵", { type: 2 });
     bot.manager.init(client.user.id);
     console.log("[Lavalink] Manager initialized");
   },
